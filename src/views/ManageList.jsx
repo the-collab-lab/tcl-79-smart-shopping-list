1a6541e7e61cdb1d4de83eeb8b47e5e4c4de6fed
wyna <queralt.guillen@gmail.com>
2024-08-20 12:43:36 +0200

    notes + item label

--- src/views/ManageList.jsx
+++ src/views/ManageList.jsx
@@ -1,7 +1,30 @@
 export function ManageList() {
 	return (
-		<p>
-			Hello from the <code>/manage-list</code> page!
-		</p>
+		/*
+			create form to add items to list
+				“Soon”, corresponding to 7 days
+				“Kind of soon”, corresponding to 14 days
+				“Not soon”, corresponding to 30 days
+
+			input that accepts the name of the item 
+			has a semantic label element associated with it
+
+			submit this form with both the mouse and the Enter key
+
+			onSubmit ->  message indicating that the item either was 
+			or was not saved to the database
+
+			The console.log in the addItem function in src/api/firebase.js is 
+			replaced with a function that adds the new document to the Firestore database. 
+			That function will be imported from the firebase/firestore module.
+
+ 			The user’s soon/not soon/kind of soon choice is used to calculate nextPurchasedDate
+		*/
+		<form action="submit">
+			<div className="item-name">
+				<label htmlFor="itemName">Enter the item name: </label>
+				<input type="text" name="itemName" id="itemName" required />
+			</div>
+		</form>
 	);
 }
