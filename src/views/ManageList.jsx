409547050be7b0df679db7dca8a8309ca3e4520c
marshjaja <114920895+marshjaja@users.noreply.github.com>
2024-08-21 15:16:09 +0100

    Form finalised, submit on enter

--- src/views/ManageList.jsx
+++ src/views/ManageList.jsx
@@ -1,39 +1,56 @@
-export function ManageList() {
+import { useState } from 'react';
+import { addItem } from '../api/firebase';
+export function ManageList({ listPath }) {
+	const [formData, setFormData] = useState({
+		itemName: '',
+		daysUntilNextPurchase: '',
+	});
+
+	const handleSubmit = async (event) => {
+		console.log(formData, 'inside submit');
+		try {
+			event.preventDefault();
+			await addItem(listPath, formData);
+			alert(`${formData.itemName} was added to the list successfully`);
+		} catch (error) {
+			console.error('error', error);
+			alert(`There was a problem adding ${formData.itemName} to the list`);
+		}
+	};
+
+	const handleChange = (event) => {
+		console.log(formData);
+		let { name, value } = event.target;
+		setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+	};
+
 	return (
-		/*
-			create form to add items to list
-				“Soon”, corresponding to 7 days
-				“Kind of soon”, corresponding to 14 days
-				“Not soon”, corresponding to 30 days
-
-			input that accepts the name of the item 
-			has a semantic label element associated with it
-
-			submit this form with both the mouse and the Enter key
-
-			onSubmit ->  message indicating that the item either was 
-			or was not saved to the database
-
-			The console.log in the addItem function in src/api/firebase.js is 
-			replaced with a function that adds the new document to the Firestore database. 
-			That function will be imported from the firebase/firestore module.
-
- 			The user’s soon/not soon/kind of soon choice is used to calculate nextPurchasedDate
-		*/
-		<form action="submit">
+		<form onSubmit={(event) => handleSubmit(event)}>
 			<div className="item-name">
 				<label htmlFor="itemName">Enter the item name: </label>
-				<input type="text" name="itemName" id="itemName" required />
+				<input
+					type="text"
+					name="itemName"
+					id="itemName"
+					required
+					value={formData.itemName}
+					onChange={handleChange}
+				/>
 			</div>
 			<div className="buy-again">
-				<label htmlFor="buyAgain">
+				<label htmlFor="daysUntilNextPurchase">
 					When do you think you will need to purchase this item again?
 				</label>
-				<select name="buyAgain" id="buyAgain">
-					{/*replace values with numbers?*/}
-					<option value="soon">Soon (7 days)</option>
-					<option value="kindOfSoon">Kind of soon (14 days)</option>
-					<option value="notSoon">Not soon (30 days)</option>
+				<select
+					name="daysUntilNextPurchase"
+					id="daysUntilNextPurchase"
+					value={formData.daysUntilNextPurchase}
+					onChange={handleChange}
+				>
+					<option value="">Select Time</option>
+					<option value="7">Soon (7 days)</option>
+					<option value="14">Kind of soon (14 days)</option>
+					<option value="30">Not soon (30 days)</option>
 				</select>
 			</div>
 			<button type="submit">Add Item</button>
