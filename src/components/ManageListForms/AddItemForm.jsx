11ca0092cb8cf412269a526a6c16a2b0334c9e4f
Huda Mabkhoot <hudamabkhoot@gmail.com>
2024-09-10 17:23:23 +0300

    refactored code for bug fix

--- src/components/ManageListForms/AddItemForm.jsx
+++ src/components/ManageListForms/AddItemForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { addItem } from '../../api/firebase';
+import toast from 'react-hot-toast';
 
 export default function AddItemForm({ listPath, data }) {
 	const [formData, setFormData] = useState({
@@ -9,19 +10,31 @@ export default function AddItemForm({ listPath, data }) {
 
 	const handleSubmit = async (event) => {
 		event.preventDefault();
+		// (for later) maybe simplified the replace and added trim() to remove spaces like this:
+		// .toLowerCase().replace(/[^\w]/g, '').trim();
+		// (for later) maybe make this a function and move it into utils
+		const formattedItemName = formData.itemName
+			.toLowerCase()
+			.replace(/[.,/#!$%^&*;:{}=\-_~()\s]/g, '')
+			.replace(/[^\w\s]/gi, '');
+
+		console.log('formattedItemName', formattedItemName);
+
 		try {
-			console.log(formData.itemName);
-			const submittedItem = formData.itemName
-				.toLowerCase()
-				.replace(/[.,/#!$%^&*;:{}=\-_`~()\s]/g, '')
-				.replace(/[^\w\s]/gi, '');
-			setFormData((prevData) => ({
-				...prevData,
-				itemName: submittedItem,
-			}));
-			console.log('after set form data,', submittedItem);
-			await addItem(listPath, formData);
-			alert(`${formData.itemName} was added to the list successfully`);
+			// (for later) replace .filter with .some to make this true or false because it didnt work in some cases
+			const filteredList = data?.filter((item) => {
+				item.name === formData.itemName;
+			});
+
+			console.log('filteredList', filteredList);
+
+			if (filteredList.length === 0) {
+				// (done) replaced setFormData we had before with { ...formData, itemName: formattedItemName }
+				await addItem(listPath, { ...formData, itemName: formattedItemName });
+				alert(`${formattedItemName} was added to the list successfully`);
+			} else {
+				toast.error(`${formattedItemName} is already on your list`);
+			}
 		} catch (error) {
 			alert(`There was a problem adding ${formData.itemName} to the list`);
 		}
