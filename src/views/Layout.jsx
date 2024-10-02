ea7678afc5862b4c1fe0d93e9cd813117877e151
marshjaja <114920895+marshjaja@users.noreply.github.com>
2024-10-02 16:25:38 +0100

    feat: Implement initial dark and light mode with matching black and white logos

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { useState } from 'react';
 import { Outlet, NavLink } from 'react-router-dom';
 
 import './Layout.css';
@@ -6,7 +7,7 @@ import { useAuth } from '../api';
 import { SignInButton, SignOutButton } from '../api/useAuth';
 import { Toaster } from 'react-hot-toast';
 import { Button } from '@/components/ui/button';
-import { House, ListPlus } from 'lucide-react';
+import { Eclipse, House, ListPlus, Sun } from 'lucide-react';
 
 /**
  * TODO: The links defined in this file don't work!
@@ -20,74 +21,118 @@ export function Layout() {
 	const { user } = useAuth();
 	const linkClass = ({ isActive }) =>
 		isActive
-			? 'bg-pink-500 text-red-500 hover:bg-pink-500 hover:text-white rounded-2xl px-3 py-2 p-4'
-			: 'text-yellow-500 hover:bg-pink-500 hover:text-white rounded-2xl px-3 py-2 p-4';
+			? 'bg-green-200 hover:bg-green-200 dark:bg-green-400 dark:hover:bg-green-400  rounded-2xl px-3 py-2 p-4'
+			: 'hover:bg-green-200 dark:hover:bg-green-400 hover:text-white rounded-2xl px-3 py-2 p-4';
+
+	const [darkMode, setDarkMode] = useState(false);
+
+	const toggleDarkMode = () => {
+		setDarkMode(!darkMode);
+	};
 
 	return (
 		<>
-			<div className="Layout">
-				<header className="Layout-header">
-					{/* Desktop top nav here */}
-					<div className="hidden lg:flex items-center justify-between p-4">
-						<div className="w-full lg:w-auto text-center lg:text-left">
-							<NavLink to="/" className="Nav-link">
-								<img
-									src="/src/LOGO-FRAME.png"
-									alt="GrocerEase logo"
-									className="Logo mx-auto lg:mx-0"
-								/>
-							</NavLink>
+			<div className={`${darkMode && 'dark'}`}>
+				<div className="Layout text-black dark:text-white bg-white dark:bg-slate-950">
+					<header className="Layout-header">
+						{/* Desktop top nav here */}
+						<div className="hidden lg:flex items-center justify-between p-4">
+							<div className="w-full lg:w-auto text-center lg:text-left">
+								<NavLink to="/" className="Nav-link">
+									{darkMode ? (
+										<img
+											src="/src/LOGO-white.png"
+											alt="GrocerEase logo"
+											className="Logo mx-auto lg:mx-0"
+										/>
+									) : (
+										<img
+											src="/src/LOGO-black.png"
+											alt="GrocerEase logo"
+											className="Logo mx-auto lg:mx-0"
+										/>
+									)}
+								</NavLink>
+							</div>
+							<div className="flex items-center space-x-4">
+								{!!user && <p>Welcome, {user.displayName}</p>}
+								<NavLink to="/" className={linkClass}>
+									<Button
+										variant="link"
+										className="text-black dark:text-white text-base"
+									>
+										Home
+									</Button>
+								</NavLink>
+								<NavLink to="/list" className={linkClass}>
+									<Button
+										variant="link"
+										className="text-black dark:text-white text-base"
+									>
+										List
+									</Button>
+								</NavLink>
+								{!!user ? <SignOutButton /> : <SignInButton />}
+								<Button
+									onClick={toggleDarkMode}
+									className="absolute w-16 bottom-16 right-16 p-2 rounded-full text-pink-600  font-semibold"
+								>
+									{darkMode ? <Eclipse /> : <Sun />}
+								</Button>
+							</div>
 						</div>
-						<div className="flex items-center space-x-4">
-							{!!user && <p>Welcome, {user.displayName}</p>}
+
+						{/* Mobile/Tablet bottom nav here */}
+						<div className="flex lg:hidden items-center justify-between p-4">
+							<NavLink to="/" className="Nav-link">
+								{darkMode ? (
+									<img
+										src="/src/LOGO-white.png"
+										alt="GrocerEase logo"
+										className="Logo mx-auto lg:mx-0"
+									/>
+								) : (
+									<img
+										src="/src/LOGO-black.png"
+										alt="GrocerEase logo"
+										className="Logo mx-auto lg:mx-0"
+									/>
+								)}
+							</NavLink>
+
+							<div className="flex items-center space-x-2 ">
+								{!!user ? <SignOutButton /> : <SignInButton />}
+								<Button
+									onClick={toggleDarkMode}
+									className=" p-2  text-pink-600 rounded-full  font-semibold"
+								>
+									{darkMode ? <Eclipse /> : <Sun />}
+								</Button>
+							</div>
+						</div>
+					</header>
+
+					<main className="Layout-main">
+						<Outlet />
+					</main>
+
+					{/* Mobile/Tablet Bottom Nav here */}
+					<nav className="block lg:hidden fixed bottom-0 left-0 right-0 shadow-lg p-4 ">
+						<div className="flex justify-around items-center">
 							<NavLink to="/" className={linkClass}>
-								<Button variant="link" className="text-white text-base">
-									Home
+								<Button className="text-black dark:text-white text-base">
+									<House />
 								</Button>
 							</NavLink>
 							<NavLink to="/list" className={linkClass}>
-								<Button variant="link" className="text-white text-base">
-									List
+								<Button className="text-black dark:text-white text-base">
+									<ListPlus />
 								</Button>
 							</NavLink>
-							{!!user ? <SignOutButton /> : <SignInButton />}
 						</div>
-					</div>
-
-					{/* Mobile/Tablet bottom nav here */}
-					<div className="flex lg:hidden items-center justify-between p-4">
-						<NavLink to="/" className="Nav-link">
-							<img
-								src="/src/LOGO-FRAME.png"
-								alt="GrocerEase logo"
-								className="Logo mx-auto lg:mx-0"
-							/>
-						</NavLink>
-
-						{!!user ? <SignOutButton /> : <SignInButton />}
-					</div>
-				</header>
-
-				<main className="Layout-main">
-					<Outlet />
-				</main>
-
-				{/* Mobile/Tablet Bottom Nav here */}
-				<nav className="block lg:hidden fixed bottom-0 left-0 right-0 shadow-lg p-4 text-white">
-					<div className="flex justify-around items-center">
-						<NavLink to="/" className={linkClass}>
-							<Button className="text-white text-base">
-								<House />
-							</Button>
-						</NavLink>
-						<NavLink to="/list" className={linkClass}>
-							<Button className="text-white text-base">
-								<ListPlus />
-							</Button>
-						</NavLink>
-					</div>
-				</nav>
-				<Toaster />
+					</nav>
+					<Toaster />
+				</div>
 			</div>
 		</>
 	);
