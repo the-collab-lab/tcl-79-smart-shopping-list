1e47a4766a7159948e9a4de7a20167975a8be3d5
marshjaja <114920895+marshjaja@users.noreply.github.com>
2024-10-02 12:48:22 +0100

    fix: Fix the visibility issue of the Sign In element on tablet and mobile devices, add icons to navbar to match the design

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -6,6 +6,7 @@ import { useAuth } from '../api';
 import { SignInButton, SignOutButton } from '../api/useAuth';
 import { Toaster } from 'react-hot-toast';
 import { Button } from '@/components/ui/button';
+import { House, ListPlus } from 'lucide-react';
 
 /**
  * TODO: The links defined in this file don't work!
@@ -21,11 +22,13 @@ export function Layout() {
 		isActive
 			? 'bg-pink-500 text-red-500 hover:bg-pink-500 hover:text-white rounded-2xl px-3 py-2 p-4'
 			: 'text-yellow-500 hover:bg-pink-500 hover:text-white rounded-2xl px-3 py-2 p-4';
+
 	return (
 		<>
 			<div className="Layout">
 				<header className="Layout-header">
-					<div className="flex items-center justify-between p-4">
+					{/* Desktop top nav here */}
+					<div className="hidden lg:flex items-center justify-between p-4">
 						<div className="w-full lg:w-auto text-center lg:text-left">
 							<NavLink to="/" className="Nav-link">
 								<img
@@ -35,7 +38,7 @@ export function Layout() {
 								/>
 							</NavLink>
 						</div>
-						<div className="hidden lg:flex items-center space-x-4">
+						<div className="flex items-center space-x-4">
 							{!!user && <p>Welcome, {user.displayName}</p>}
 							<NavLink to="/" className={linkClass}>
 								<Button variant="link" className="text-white text-base">
@@ -47,25 +50,21 @@ export function Layout() {
 									List
 								</Button>
 							</NavLink>
-							{!!user ? (
-								<>
-									<SignOutButton>
-										<Button variant="outline">Sign Out</Button>
-									</SignOutButton>
-								</>
-							) : (
-								<SignInButton>
-									<Button variant="outline">Sign In</Button>
-								</SignInButton>
-							)}
+							{!!user ? <SignOutButton /> : <SignInButton />}
 						</div>
-						{!!user && (
-							<div className="block lg:hidden">
-								<SignOutButton>
-									<Button variant="outline">Sign Out</Button>
-								</SignOutButton>
-							</div>
-						)}
+					</div>
+
+					{/* Mobile/Tablet bottom nav here */}
+					<div className="flex lg:hidden items-center justify-between p-4">
+						<NavLink to="/" className="Nav-link">
+							<img
+								src="/src/LOGO-FRAME.png"
+								alt="GrocerEase logo"
+								className="Logo mx-auto lg:mx-0"
+							/>
+						</NavLink>
+
+						{!!user ? <SignOutButton /> : <SignInButton />}
 					</div>
 				</header>
 
@@ -73,15 +72,17 @@ export function Layout() {
 					<Outlet />
 				</main>
 
-				{/* Mobile/Tablet bottom nav here */}
+				{/* Mobile/Tablet Bottom Nav here */}
 				<nav className="block lg:hidden fixed bottom-0 left-0 right-0 shadow-lg p-4 text-white">
 					<div className="flex justify-around items-center">
 						<NavLink to="/" className={linkClass}>
-							<Button className="text-white text-base">Home</Button>
+							<Button className="text-white text-base">
+								<House />
+							</Button>
 						</NavLink>
 						<NavLink to="/list" className={linkClass}>
-							<Button variant="link" className="text-white text-base">
-								List
+							<Button className="text-white text-base">
+								<ListPlus />
 							</Button>
 						</NavLink>
 					</div>
