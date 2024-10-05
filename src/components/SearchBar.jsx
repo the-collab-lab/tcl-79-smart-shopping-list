fde8c2a51dbd8e21dbb9bf0f45f493b1f806c84c
Huda Mabkhoot <hudamabkhoot@gmail.com>
2024-10-06 00:48:49 +0300

    Fixed the layout of list page

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import { Input } from './ui/input';
+import { ListFilter } from 'lucide-react';
 
 export function SearchBar({ allData, setDisplayData, setSearch, search }) {
 	const handleInputChange = (e) => {
@@ -13,13 +14,17 @@ export function SearchBar({ allData, setDisplayData, setSearch, search }) {
 	};
 
 	return (
-		<form className="text-black">
+		<form className="relative w-full">
+			<span className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+				<ListFilter className="h-5 w-5 text-grey" />
+			</span>
 			<Input
 				type="text"
 				id="item-filter"
 				value={search}
 				placeholder="Search..."
 				onChange={handleInputChange}
+				className="border-[1px] rounded-[5px] text-[1em] h-[3rem] pl-10 focus-visible:outline-none focus:ring-1 focus:ring-green-500 text-black"
 			/>
 		</form>
 	);
