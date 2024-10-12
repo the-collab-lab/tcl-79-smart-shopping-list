d866df7c0669866670e9976f403a3e78d84e01d8
Huda Mabkhoot <hudamabkhoot@gmail.com>
2024-10-12 15:10:48 +0300

    Created item edit feature

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import { ONE_DAY_IN_MILLISECONDS } from '../utils/dates';
 import toast from 'react-hot-toast';
 import { Button } from './ui/button';
-import { Trash2 } from 'lucide-react';
+import { Trash2, Pencil } from 'lucide-react';
 import { getIndicatorColor } from '../utils/helpers';
 import {
 	AlertDialog,
@@ -16,6 +16,16 @@ import {
 	AlertDialogTitle,
 	AlertDialogTrigger,
 } from './ui/alert-dialog';
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from '@/components/ui/dialog';
+import EditItemForm from './ManageListForms/EditItemForm';
 
 export function ListItem({
 	listPath,
@@ -28,6 +38,9 @@ export function ListItem({
 	dayInterval,
 	dateCreated,
 	indicator,
+	isOpen,
+	handleOpenModal,
+	dateNextPurchased,
 }) {
 	const [isAlertOpen, setIsAlertOpen] = useState(false);
 
@@ -94,38 +107,64 @@ export function ListItem({
 				<div className={`${getIndicatorColor(indicator)} rounded-[5px] px-3`}>
 					<p className="capitalize justify-self-end text-base">{indicator}</p>
 				</div>
-				<AlertDialog open={isAlertOpen} onOpenChange={setIsAlertOpen}>
-					<AlertDialogTrigger asChild>
-						<Button
-							className="bg-transparent hover:bg-transparent"
-							type="button"
-							id={id}
-							onClick={() => setIsAlertOpen(true)}
-						>
-							<Trash2 className="text-primary-green w-6 h-6 md:w-7 md:h-7" />
-						</Button>
-					</AlertDialogTrigger>
-					<AlertDialogContent>
-						<AlertDialogHeader>
-							<AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-							<AlertDialogDescription>
-								This action cannot be undone. Do you really want to delete{' '}
-								{name}?
-							</AlertDialogDescription>
-						</AlertDialogHeader>
-						<AlertDialogFooter>
-							<AlertDialogCancel onClick={() => setIsAlertOpen(false)}>
-								Cancel
-							</AlertDialogCancel>
-							<AlertDialogAction
-								onClick={handleDelete}
-								className="bg-primary-pink text-white"
+				<div className="flex items-center p-2">
+					<AlertDialog open={isAlertOpen} onOpenChange={setIsAlertOpen}>
+						<AlertDialogTrigger asChild>
+							<Button
+								className="bg-transparent hover:bg-transparent"
+								type="button"
+								id={id}
+								onClick={() => setIsAlertOpen(true)}
 							>
-								Continue
-							</AlertDialogAction>
-						</AlertDialogFooter>
-					</AlertDialogContent>
-				</AlertDialog>
+								<Trash2 className="text-primary-green w-6 h-6 md:w-7 md:h-7" />
+							</Button>
+						</AlertDialogTrigger>
+						<AlertDialogContent>
+							<AlertDialogHeader>
+								<AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+								<AlertDialogDescription>
+									This action cannot be undone. Do you really want to delete{' '}
+									{name}?
+								</AlertDialogDescription>
+							</AlertDialogHeader>
+							<AlertDialogFooter>
+								<AlertDialogCancel onClick={() => setIsAlertOpen(false)}>
+									Cancel
+								</AlertDialogCancel>
+								<AlertDialogAction
+									onClick={handleDelete}
+									className="bg-primary-pink text-white"
+								>
+									Continue
+								</AlertDialogAction>
+							</AlertDialogFooter>
+						</AlertDialogContent>
+					</AlertDialog>
+					<Dialog open={isOpen} onOpenChange={() => handleOpenModal(id)}>
+						<DialogTrigger asChild>
+							<Button className="bg-transparen hover:bg-transparen p-0">
+								<Pencil className="text-primary-green w-6 h-6 md:w-7 md:h-7" />
+							</Button>
+						</DialogTrigger>
+						<DialogContent>
+							<DialogHeader>
+								<DialogTitle>Edit {name}</DialogTitle>
+								<DialogDescription>
+									Modify the details of the item you&apos;d like to edit.
+								</DialogDescription>
+							</DialogHeader>
+							<EditItemForm
+								listPath={listPath}
+								name={name}
+								id={id}
+								quantity={quantity}
+								dateNextPurchased={dateNextPurchased}
+								handleOpenModal={handleOpenModal}
+							/>
+							<DialogFooter className="sm:justify-start"></DialogFooter>
+						</DialogContent>
+					</Dialog>
+				</div>
 			</div>
 		</li>
 	);
---
+++ src/components/ManageListForms/EditItemForm.jsx
@@ -0,0 +1,178 @@
+import { useEffect, useState } from 'react';
+import { editItem } from '../../api/firebase';
+import toast from 'react-hot-toast';
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
+import { getFutureDate } from '../../utils/dates';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+
+export default function EditItemForm({
+	listPath,
+	id,
+	name,
+	quantity,
+	dateNextPurchased,
+	handleOpenModal,
+}) {
+	const [formData, setFormData] = useState({
+		itemName: name || '',
+		itemQuantity: quantity || 1,
+		daysUntilNextPurchase: dateNextPurchased || '',
+	});
+
+	useEffect(() => {
+		setFormData({
+			itemName: name,
+			itemQuantity: quantity,
+			daysUntilNextPurchase: dateNextPurchased,
+		});
+	}, [name]);
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+
+		const formattedItemName = formData.itemName
+			.toLowerCase()
+			.replace(/^\s\s*/, '')
+			.replace(/\s\s*$/, '')
+			.replace(/[^a-zA-Z ]/g, '');
+
+		const formQuantity = parseInt(formData.itemQuantity, 10);
+		const newDateNextPurchased = formData.daysUntilNextPurchase;
+
+		if (formattedItemName.length === 0) {
+			toast.error(`No numbers or special characters.`);
+			resetItemName();
+			return;
+		}
+
+		const hasChanged =
+			formattedItemName !== name ||
+			formQuantity !== quantity ||
+			newDateNextPurchased !== dateNextPurchased;
+		if (!hasChanged) {
+			toast.error('No changes were made.');
+			handleOpenModal();
+			return;
+		}
+
+		try {
+			let updatedData = {
+				itemName: formattedItemName,
+				itemQuantity: formQuantity,
+			};
+
+			if (newDateNextPurchased !== dateNextPurchased) {
+				const futureDate = parseInt(newDateNextPurchased);
+				const formattedFutureDate = getFutureDate(futureDate);
+				updatedData.dateNextPurchased = formattedFutureDate;
+			} else {
+				updatedData.dateNextPurchased = dateNextPurchased;
+			}
+			console.log();
+			handleOpenModal();
+			await editItem(listPath, id, updatedData);
+			toast.success(`${formattedItemName} has been updated!`);
+		} catch (error) {
+			toast.error(`Failed to edit ${formattedItemName}`);
+		}
+	};
+
+	const handleChange = (event) => {
+		let { name, value } = event.target;
+		setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+	};
+
+	const handleRadioChange = (value) => {
+		setFormData((prevFormData) => ({
+			...prevFormData,
+			daysUntilNextPurchase: value,
+		}));
+	};
+
+	return (
+		<form
+			className="flex flex-col items-center gap-8 max-w-[400px] text-black"
+			onSubmit={(event) => handleSubmit(event)}
+		>
+			<div className="flex flex-col gap-2 w-full">
+				<label htmlFor="itemName" className="text-md font-medium">
+					Item name{' '}
+				</label>
+				<Input
+					type="text"
+					name="itemName"
+					id="itemName"
+					placeholder="Enter item name"
+					required
+					value={formData.itemName}
+					onChange={handleChange}
+				/>
+			</div>
+			<div className="flex flex-col gap-2 w-full">
+				<label htmlFor="daysUntilNextPurchase" className="text-md font-medium">
+					How soon would you like to buy this again?
+				</label>
+				<RadioGroup
+					onValueChange={handleRadioChange}
+					className="flex my-2 items-center justify-center gap-4"
+					id="daysUntilNextPurchase"
+				>
+					<div className="flex flex-col items-center justify-center rounded-xl border border-light-pink gap-4 w-28 h-28 shadow-bottom-right transition-transform duration-200 ease-in-out transform active:scale-95">
+						<RadioGroupItem
+							value="7"
+							id="soon"
+							name="timeFrame"
+							className="border  border-soon text-soon"
+						/>
+						<label htmlFor="soon" className="font-semibold text-sm">
+							Soon
+						</label>
+					</div>
+					<div className="flex flex-col items-center justify-center rounded-xl border border-light-pink gap-4 w-28 h-28 shadow-bottom-right transition-transform duration-200 ease-in-out transform active:scale-95">
+						<RadioGroupItem
+							value="14"
+							id="kind-of-soon"
+							name="timeFrame"
+							className="border border-kind-of-soon text-kind-of-soon"
+						/>
+						<label htmlFor="kind-of-soon" className="font-semibold text-sm">
+							Kind of soon
+						</label>
+					</div>
+					<div className="flex flex-col items-center justify-center rounded-xl border border-light-pink gap-4 w-28 h-28 shadow-bottom-right transition-transform duration-200 ease-in-out transform active:scale-95">
+						<RadioGroupItem value="30" id="not-of-soon" name="timeFrame" />
+						<label htmlFor="not of soon" className="font-semibold text-sm">
+							Not soon
+						</label>
+					</div>
+				</RadioGroup>
+			</div>
+			<div className="flex flex-col items-start gap-2 w-full">
+				<label htmlFor="quantity" className="text-md font-medium">
+					Quantity
+				</label>
+				<div className="flex items-start">
+					<Input
+						type="number"
+						name="itemQuantity"
+						id="quantity"
+						required
+						value={formData.itemQuantity}
+						onChange={handleChange}
+						min={1}
+						max={100}
+					/>
+				</div>
+			</div>
+			<div className="flex w-full">
+				<Button
+					type="submit"
+					className="bg-primary-pink text-white rounded-xl w-full hover:bg-primary-pink hover:bg-opacity-75 text-sm"
+				>
+					Edit Item
+				</Button>
+			</div>
+		</form>
+	);
+}
